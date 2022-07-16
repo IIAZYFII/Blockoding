5c131fcc80af0110f0309097fed7d3886c505b24
IIAZYFII <18hussainasif@gmail.com>
2022-07-16 14:07:25 +0100

    added icons

--- src/main/java/com/azyf/finalyearproject/StageInitializer.java
+++ src/main/java/com/azyf/finalyearproject/StageInitializer.java
@@ -2,15 +2,21 @@ package com.azyf.finalyearproject;
 
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class StageInitializer implements ApplicationListener<BlockApplication.StageReadyEvent> {
     private Canvas canvas;
     private Canvas blockCanvas;
+    private Image playButtonImg;
+    private Image stopButtonImg;
     @Override
     public void onApplicationEvent(BlockApplication.StageReadyEvent event) {
         BorderPane root = (BorderPane) buildGUI();
@@ -40,7 +46,7 @@ public class StageInitializer implements ApplicationListener<BlockApplication.St
         AnchorPane.setLeftAnchor(ioBar, 10d);
         AnchorPane.setRightAnchor(tabBar,10d);
 
-        HBox topBar = new HBox();
+      HBox topBar = new HBox();
         root.setTop(topBar);
 
         HBox rightPane = new HBox();
@@ -50,9 +56,19 @@ public class StageInitializer implements ApplicationListener<BlockApplication.St
         rightPane.getChildren().add(spriteBox);
         rightPane.getChildren().add(blockCanvas);
 
+       Button playButton = new Button();
+       //playButton.setStyle("-fx-background-color: transparent;");
+       playButtonImg = new Image("C:\\Users\\hussa\\Dropbox\\Computer Science\\Year 3\\Final Year Project\\FinalYearProject\\Assets\\Images\\Playbutton.png");
+       ImageView playButtonView = new ImageView(playButtonImg);
+       playButton.setGraphic(playButtonView);
+       topBar.getChildren().add(playButton);
 
-
-
+       Button stopButton = new Button();
+       //stopButton.setStyle("-fx-background-color: transparent;");
+       stopButtonImg = new Image("C:\\Users\\hussa\\Dropbox\\Computer Science\\Year 3\\Final Year Project\\FinalYearProject\\Assets\\Images\\Stopbutton.png");
+       ImageView stopButtonView = new ImageView(stopButtonImg);
+       stopButton.setGraphic(stopButtonView);
+       topBar.getChildren().add(stopButton);
 
 
 
