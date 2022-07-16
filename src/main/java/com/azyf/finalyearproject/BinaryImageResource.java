f9255cd75c5b670f51973b0e88b9bf9194317220
IIAZYFII <18hussainasif@gmail.com>
2022-07-16 14:22:24 +0100

    added method to receieve an image using REST API

--- src/main/java/com/azyf/finalyearproject/BinaryImageResource.java
+++ src/main/java/com/azyf/finalyearproject/BinaryImageResource.java
@@ -1,4 +1,29 @@
 package com.azyf.finalyearproject;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@RestController
 public class BinaryImageResource {
+
+    @PostMapping(value = "sendImage", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void receiveBinaryImage(@RequestBody BinaryImage binaryImage) throws IOException {
+        if (binaryImage != null) {
+            //Converts image into bytes from string
+            byte[] imageBytes = java.util.Base64.getDecoder().decode(binaryImage.getImageContent());
+
+            //Creates a stream so bytes are readable
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(imageBytes);
+
+            //Reads the bytes from the input stream and sets it to the image
+            BufferedImage imageFromBytes = ImageIO.read(inputStream);
+        }
+    }
 }
