ec638d8a3135743ebfc6865d8744143a0ff2bea5
IIAZYFII <18hussainasif@gmail.com>
2022-07-16 14:11:41 +0100

    set up BinaryImage Class

--- src/main/java/com/azyf/finalyearproject/BinaryImage.java
+++ src/main/java/com/azyf/finalyearproject/BinaryImage.java
@@ -1,4 +1,25 @@
 package com.azyf.finalyearproject;
 
 public class BinaryImage {
+    private int width;
+    private int height;
+    private String imageContent;
+
+    public BinaryImage(int width, int height, String imageContent) {
+        this.width = width;
+        this.height = height;
+        this.imageContent = imageContent;
+    }
+
+    public String getImageContent() {
+        return imageContent;
+    }
+    @Override
+    public String toString() {
+        return "BinaryImage{" +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
 }
