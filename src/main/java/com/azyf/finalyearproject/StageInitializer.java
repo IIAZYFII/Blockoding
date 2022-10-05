e7ba9eb722cfc9ac9f3d0a31d0b4072e22d559b3
IIAZYFII <18hussainasif@gmail.com>
2022-10-05 09:59:57 +0100

    started implementing QR Code

--- src/main/java/com/azyf/finalyearproject/StageInitializer.java
+++ src/main/java/com/azyf/finalyearproject/StageInitializer.java
@@ -1,17 +1,33 @@
 package com.azyf.finalyearproject;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.*;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.checkerframework.checker.units.qual.N;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 @Component
@@ -86,6 +102,80 @@ public class StageInitializer implements ApplicationListener<BlockApplication.St
             }
         });
 
+        /*
+        MenuBar menuBar = new MenuBar();
+        Menu file = new Menu("File");
+        MenuItem settings = new MenuItem("Settings");
+        file.getItems().add(settings);
+        menuBar.getMenus().add(file);
+        root.getChildren().add(menuBar);
+         */
+        Button settingButton = new Button();
+        Image settingButtonImg = new Image("C:\\Users\\hussa\\Dropbox\\Computer Science\\Year 3\\Final Year Project\\FinalYearProject\\Assets\\Images\\SettingsButton.png");
+        ImageView settingButtonView = new ImageView(settingButtonImg);
+        settingButton.setGraphic(settingButtonView);
+        topBar.getChildren().add(settingButton);
+        settingButton.setOnAction(e -> {
+            drawSettings();
+        });
         return  root;
+    }
+
+    private void drawSettings(){
+        Stage settingStage = new Stage();
+        BorderPane root = new BorderPane();
+
+        root.setStyle("-fx-background-color: #96ffa1;");
+
+
+        Button linkAppButton = new Button();
+        linkAppButton.setMinSize(100,100);
+        linkAppButton.setText("Link App");
+        linkAppButton.setOnAction(e -> {
+            try {
+                generateQRCode();
+                root.getChildren().clear();
+                Image qrCode = new Image("C:\\Users\\hussa\\Dropbox\\Computer Science\\Year 3\\Final Year Project\\FinalYearProject\\Cache");
+                ImageView qrCodeViewer = new ImageView(qrCode);
+                root.getChildren().add(qrCodeViewer);
+
+            } catch (WriterException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        HBox hBox = new HBox();
+        hBox.getChildren().add(linkAppButton);
+        root.getChildren().add(hBox);
+        Scene scene = new Scene(root, 350,350);
+        settingStage.setScene(scene);
+        settingStage.showAndWait();
+
+    }
+
+    private void generateQRCode() throws WriterException, IOException {
+        String localIP = InetAddress.getLocalHost().getHostAddress();
+        System.out.println(localIP);
+        QRCodeWriter qrCodeGenerator = new QRCodeWriter();
+        BitMatrix bitMatrix =
+                qrCodeGenerator.encode(localIP, BarcodeFormat.QR_CODE,100,100);
+        WritableImage writableImage = new WritableImage(100,100);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+        System.out.println(bitMatrix.toString());
+        for(int j = 0; j < 100; j++) {
+            for(int i = 0; i < 100; i++) {
+                if(bitMatrix.get(i,j) == true) {
+                    pixelWriter.setColor(i,j, Color.color(0.0,0.0,0.0));
+                } else {
+                    pixelWriter.setColor(i,j, Color.color(1.0,1.0,1.0));
+                }
+
+            }
+        }
+        File saveImage = new File("C:\\Users\\hussa\\Dropbox\\Computer Science\\Year 3\\Final Year Project\\FinalYearProject\\Cache\\qrcode.png");
+        BufferedImage image = SwingFXUtils.fromFXImage(writableImage, null);
+        ImageIO.write(image, "png", saveImage);
+
     }
 }
