67ae53ba02ec95500f804d011df8743c556d3d00
IIAZYFII <18hussainasif@gmail.com>
2022-11-20 15:33:03 +0000

    Cleaned up ParseTree.java added javadocs and removed irrelevant code and comments

--- src/main/java/com/azyf/finalyearproject/ParseTree.java
+++ src/main/java/com/azyf/finalyearproject/ParseTree.java
@@ -1,6 +1,4 @@
 package com.azyf.finalyearproject;
-import org.apache.pdfbox.debugger.ui.Tree;
-import org.w3c.dom.Node;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,18 +6,28 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-
+/**
+ * This class represent the tangible programming language using the tree data structure.
+ *
+ * @author  Hussain Asif
+ */
 public class  ParseTree {
     public TreeNode root = null;
-    private final static int NUMBER_OF_BLOCKS = 51;
-    private TreeNode currentNode = null;
+    private TreeNode currentParentNode = null;
     private HashMap<String,Block> Blocks = new HashMap<>();
 
+    /**
+     * An empty constructor to create an empty Parse Tree
+     * @throws FileNotFoundException
+     */
     public ParseTree() throws FileNotFoundException {
         loadBlocks();
     }
 
-
+    /**
+     * Loads blocks inside the dictionary.
+     * @throws FileNotFoundException
+     */
     private void loadBlocks() throws FileNotFoundException {
         File blockFile = new File("C:\\Users\\hussa\\Dropbox\\Computer Science\\Year 3\\Final Year Project\\FinalYearProject\\Assets\\Blocks\\Default.txt");
         Scanner in = null;
@@ -45,7 +53,11 @@ public class  ParseTree {
 
     }
 
-
+    /**
+     * Starts constructing the tree and filling in the parse tree.
+     * @param treeData The data that the tree will be constructed from.
+     * @throws UnsupportedEncodingException
+     */
     public void initialiseParseTree(File treeData) throws UnsupportedEncodingException {
         Scanner in = null;
         String parseTree = "";
@@ -58,119 +70,61 @@ public class  ParseTree {
         }
         System.out.println(parseTree);
         in = new Scanner(parseTree);
-        int currentBlockIndex  = 0;
         while(in.hasNext()) {
             String data = in.next();
             if(root == null) {
                 Block rootBlock = Blocks.get(data);
                 TreeNode rootNode = new TreeNode(rootBlock);
                 root = rootNode;
-                currentNode = root;
+                currentParentNode = root;
             } else if (data.equals("->")) {
                String childNotation = in.next();
                 if(childNotation.equals("[")) {
                     String  blockData = in.next();
                     Block currentBlock = Blocks.get(blockData);
                     TreeNode newNode =  new TreeNode(currentBlock);
-                    currentNode.getChildren().add(newNode);
-                    currentNode = newNode;
+                    currentParentNode.getChildren().add(newNode);
+                    currentParentNode = newNode;
                 } else {
                     throw new UnsupportedEncodingException("expected [" + "actually encoded: " + childNotation);
                 }
             } else if (data.equals("]")) {
-                 findParent(currentNode,root);
-                System.out.println("no more children for " +  currentNode.getNode().getName());
+                 setCurrentParentNode(currentParentNode,root);
+                System.out.println("no more children for " +  currentParentNode.getNode().getName());
 
 
             }  else {
                 Block currentBlock = Blocks.get(data);
                 TreeNode newNode =  new TreeNode(currentBlock);
-                 findParent(currentNode,root);
-                currentNode.getChildren().add(newNode);
-                currentNode = newNode;
+                 setCurrentParentNode(currentParentNode,root);
+                currentParentNode.getChildren().add(newNode);
+                currentParentNode = newNode;
             }
         }
 
     }
 
-    private void findParent(TreeNode checkNode, TreeNode position) {
+    /**
+     * Sets the current parent Node depending on the position of the latest node added.
+     * @param checkNode The child of the parent node that will be set.
+     * @param position The current position within the Tree.
+     */
+    private void setCurrentParentNode(TreeNode checkNode, TreeNode position) {
         if(checkNode == root) {
-            currentNode = root;
+            currentParentNode = root;
         } else {
             for(int i = 0; i < position.getChildren().size(); i++) {
                TreeNode treeNode = position.getChildren().get(i);
                if(checkNode == treeNode) {
-                     currentNode = position;
+                     currentParentNode = position;
                 } else if (treeNode.getChildren().size() != 0) {
-                    findParent(checkNode, treeNode);
+                    setCurrentParentNode(checkNode, treeNode);
                 }
             }
         }
     }
 
-    public void printTree(TreeNode position, int c) {
-        c++;
-        System.out.println(position.getNode().getName());
-        System.out.println("Children of " + position.getNode().getName() + " " + c ) ;
-        if(position.getChildren().size() == 0) {
-            System.out.println(position.getNode().getName() + " has no children") ;
-        }
-        for (int i = 0; i < position.getChildren().size(); i++) {
-            printTree(position.getChildren().get(i), c);
-        }
-    }
-/*
-    private void insertNodeIntoParseTree(TreeNode blockNode) {
-        Category NodeCategory =  blockNode.getNode().getCategory();
-        if(root.getChildren().size() == 0) {
-            root.addChildNode(blockNode);
-        } else {
-            int index = returnIndex(blockNode);
-            if(index == -1) {
-                root.getChildren().add(blockNode);
-            } else  {
-                root.getChildren().get(index).getChildren().add(blockNode);
-            }
-        }
 
-
-    }
-
- */
-/*
-    public int returnIndex(TreeNode nodeCategory) {
-
-        if(root.getChildren().size() == 0) {
-            return - 1;
-        } else {
-            for(int i = 0; i < root.getChildren().size(); i++) {
-                if(root.getChildren().get(i).getNode().getCategory() == nodeCategory.getNode().getCategory()) {
-                    return i;
-                }
-
-            }
-        }
-        return - 1;
-
-    }
-
- */
-
-     /*public boolean doesCategoryExist(TreeNode currentNode, Category NodeCategory) {
-        if(currentNode.getChildren() != null) {
-            if(currentNode.getNode().getCategory() == NodeCategory) {
-                return true;
-            }
-            for(int i = 0; i < currentNode.getChildren().size(); i++) {
-                this.doesCategoryExist(currentNode.getChildren().get(i), NodeCategory);
-            }
-        }
-
-        return false;
-    }
-
-
-      */
 
 
 
