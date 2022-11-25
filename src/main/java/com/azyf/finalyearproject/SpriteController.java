74c94fb0dcac65c0aa2283516c819927eeee97c6
IIAZYFII <18hussainasif@gmail.com>
2022-11-25 22:55:11 +0000

    implemented spriteController Getter,Setter and Constructor

--- src/main/java/com/azyf/finalyearproject/SpriteController.java
+++ src/main/java/com/azyf/finalyearproject/SpriteController.java
@@ -1,4 +1,21 @@
 package com.azyf.finalyearproject;
 
+import java.util.ArrayList;
+
 public class SpriteController {
+   private ArrayList<Sprite> sprites;
+
+    public SpriteController() {
+        sprites = new ArrayList<>();
+    }
+
+    public Sprite getSprite(int i) {
+       return sprites.get(i);
+    }
+
+    public void addSprite(Sprite sprite) {
+        sprites.add(sprite);
+    }
+
+
 }
